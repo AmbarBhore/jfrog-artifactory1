7f967036382cbd2bcce5b727735feb8b0eb0fa06
AmbarBhore <ambarbhore1234@gmail.com>
2025-05-21 08:58:48 +0530

    modified feature

--- src/main/java/com/example/rmmagent/controller/FeatureController.java
+++ src/main/java/com/example/rmmagent/controller/FeatureController.java
@@ -1,14 +1,14 @@
-package com.example.rmmagent.controller;
+#package com.example.rmmagent.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+#import org.springframework.http.ResponseEntity;
+#import org.springframework.web.bind.annotation.GetMapping;
+#import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class FeatureController {
+#@RestController
+#public class FeatureController {
 
-	    @GetMapping("/features")
-	        public ResponseEntity<String> getFeatureInfo() {
-			        return ResponseEntity.ok("🆕 New Feature Enabled in RMM Agent v3.2");
-				    }
-}
+#	    @GetMapping("/features")
+#	        public ResponseEntity<String> getFeatureInfo() {
+#			        return ResponseEntity.ok("🆕 New Feature Enabled in RMM Agent v3.2");
+#				    }
+#}
